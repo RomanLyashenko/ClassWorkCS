1bdf9abc3a54b4a553444b0553c58093f2388207
RomanLyashenko <159914055+RomanLyashenko@users.noreply.github.com>
2025-06-04 21:28:32 +0300

    Add files via upload

---
+++ QuizPlatform/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace QuizPlatform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Login = new System.Windows.Forms.Label();
+            this.Username1 = new System.Windows.Forms.Label();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.Password1 = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.Submit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.Login.AutoSize = true;
+            this.Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Login.Location = new System.Drawing.Point(330, 114);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(101, 38);
+            this.Login.TabIndex = 0;
+            this.Login.Text = "Login";
+            // 
+            // Username1
+            // 
+            this.Username1.AutoSize = true;
+            this.Username1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.Username1.Location = new System.Drawing.Point(300, 169);
+            this.Username1.Name = "Username1";
+            this.Username1.Size = new System.Drawing.Size(65, 15);
+            this.Username1.TabIndex = 1;
+            this.Username1.Text = "Username";
+            // 
+            // Username
+            // 
+            this.Username.Location = new System.Drawing.Point(303, 187);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(158, 20);
+            this.Username.TabIndex = 2;
+            this.Username.TextChanged += new System.EventHandler(this.Username_TextChanged);
+            // 
+            // Password1
+            // 
+            this.Password1.AutoSize = true;
+            this.Password1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.Password1.Location = new System.Drawing.Point(300, 220);
+            this.Password1.Name = "Password1";
+            this.Password1.Size = new System.Drawing.Size(61, 15);
+            this.Password1.TabIndex = 3;
+            this.Password1.Text = "Password";
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(303, 238);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(158, 20);
+            this.Password.TabIndex = 4;
+            this.Password.TextChanged += new System.EventHandler(this.Password_TextChanged);
+            // 
+            // Submit
+            // 
+            this.Submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Submit.Location = new System.Drawing.Point(337, 282);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(94, 38);
+            this.Submit.TabIndex = 5;
+            this.Submit.Text = "Login";
+            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Password1);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.Username1);
+            this.Controls.Add(this.Login);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Login;
+        private System.Windows.Forms.Label Username1;
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.Label Password1;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Button Submit;
+    }
+}
+
---
+++ QuizPlatform/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizPlatform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Username_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            string username = Username.Text;
+            string password = Password.Text; 
+            string path = @"C:\Users\Взрослая академия\Desktop\DataBase.csv";
+
+            bool isUnique = true;
+
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        int separatorIndex = line.IndexOf(':');
+                        if (separatorIndex >= 0)
+                        {
+                            string existingUsername = line.Substring(0, separatorIndex);
+                            if (existingUsername.Equals(username, StringComparison.OrdinalIgnoreCase))
+                            {
+                                isUnique = false;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (isUnique)
+            {
+                string lineToWrite = $"{username}:{password}";
+                using (StreamWriter sw = new StreamWriter(path, true))
+                {
+                    sw.WriteLine(lineToWrite);
+                }
+                MessageBox.Show("User successfully added.");
+            }
+            else
+            {
+                MessageBox.Show("This username already exists. Please choose a different one.");
+            }
+        }
+    }
+}
